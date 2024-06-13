04a923f196c76ec74d71555afa78e3e703593808
DerCube <leander.kh@posteo.de>
2024-06-13 14:47:30 +0200

    Enis

--- src/Fenster.java
+++ src/Fenster.java
@@ -2,13 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Fenster {
+    JLabel lable;
+    JPanel pan;
     public void fenster(int x,int y){
 
             JFrame f = new JFrame();
             f.setSize(100, 100);
             f.setVisible(true);
-            f.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-            f.getContentPane().setBackground(new Color((int)(Math.random()*100),(int)(Math.random()*100),(int)(Math.random()*100)));
+            f.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            pan =new JPanel();
+            lable =new JLabel("Guten tag das ist ein Muster das nichts darstellt");
+            f.add(pan);
+            pan.add(lable);
+            pan.setBackground(new Color((int)(Math.random()*100),(int)(Math.random()*100),(int)(Math.random()*100)));
             f.setLocation(x,y);
 
     }//fenster
